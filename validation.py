f519a8a9301033974eecaaf810fec55aef79209e
bluedaze <35676690+bluedaze@users.noreply.github.com>
2020-06-11 20:25:00 -0400

    Add files via upload
    Major refactoring. Only thing left is to query the database and then download.

--- validation.py
+++ validation.py
@@ -1,41 +1,75 @@
 import sys
 import time
 import re
+import datetime
+import sqlite3
 
-class Validate:
+class User_data:
 	''' Checks for valid youtube link '''
-	def __init__(self, url):
-		self.url = url
-		self.validate_link(self.url)
+	def __init__(self):
+		self.url = ""
+		self.user_link(self.url)
 
-	def link_error(self):
+	def __str__(self):
+		return "{self.url}".format(self=self)
+
+	def user_link(self, url):
 		''' Error message for invalid link. '''
-		error_message = "\n\nThis is not the link we were looking for... "
-		error_message +="\n\nThis is Google's youtube account: https://www.youtube.com/channel/UCK8sQmJBp8GCxrOtXWBpyEA" 
-		error_message +="\n\nThis is the kind of url we are looking for\n\n"
+		message = "Please provide a link to a youtube channel."
+		message += "\n\nHere is an example using Google's youtube channel: https://www.youtube.com/channel/UCK8sQmJBp8GCxrOtXWBpyEA" 
 
-		for c in error_message:
+		for c in message:
 			sys.stdout.write(c)
 			sys.stdout.flush()
 			time.sleep(.009)
-		self.url = input("Please provide a link: ")
-		self.validate_link(self.url)
+		self.url = input("\n\nPlease provide a link: ")
+		self.validate_link()
 
-	def validate_link(self, url):
+	def validate_link(self):
 		''' Validate if this is a url by sending a request to the url. '''
 		regex = re.compile(r"(youtube.com/channel/UC)(.{22})")
 		mo = regex.search(self.url)
 
 		if mo == None:
-			self.link_error()
+			self.user_link()
 		else:
 			self.url = "UU" + mo.group()[22::]
 
-	def __repr__(self):
-		return "{self.__class__.__name__}({self.url})".format(self=self)
 
-	def __str__(self):
-		return "{self.url}".format(self=self)
+def insert_data(*args):
+	insert_Sql = "INSERT INTO yt (channel_title, channel_id, uploads_id, video_id , video_date) VALUES (?, ?, ?, ?, ?)"
+	conn = sqlite3.connect('yt.db', isolation_level=None)
+	conn.execute('pragma journal_mode=wal;')
+	c = conn.cursor()
+	c.execute(insert_Sql, args)
+
+def create_db():
+	table_sql = "CREATE TABLE IF NOT EXISTS yt (channel_title TEXT, channel_id TEXT, uploads_id TEXT, video_id TEXT, video_date TEXT)"
+
+	conn = sqlite3.connect('yt.db')
+	c = conn.cursor()
+	c.execute(table_sql)
+	c.close()
+	conn.close()
+
+def query_db(market):
+	#Retrieves video_id matching today's date from the database.
+	today = datetime.datetime.now().isoformat()[0:10]
+	''' Query database for tweet markets '''
+	conn = sqlite3.connect("pidb.db", isolation_level=None)
+	conn.row_factory = sqlite3.Row
+	c = conn.cursor()
+	c.execute("SELECT bracket, buyYes, timeStamp FROM tweets WHERE marketName = ? ORDER BY bracket, timeStamp;", ( market,))
+	selected = [tuple(row) for row in c.fetchall()]
+	bracketData = {}
+	for i in range(9):
+		bracket = "B"+str(i+1)
+		prices = [(item[1]) for item in selected if item[0] == bracket]
+		timeStamp = [(item[2]) for item in selected if item[0] == bracket]
+		bracketData[bracket] = prices, timeStamp
+	c.close()
+	conn.close()
+	return bracketData
 
 if __name__ == "__main__":
-	print(Validate("http://www.youtube.com/channel/UCK8sQmJBp8GCxrOtXWBpyEA"))
+	print(User_data())
--- ytautodl.py
+++ ytautodl.py
@@ -1,60 +1,10 @@
 # Guide for getting details from youtube videos for future reference.
 # https://developers.google.com/youtube/v3/guides/implementation/videos
 import requests
-import datetime
 import subprocess
 import argparse
 from apikey import apikey
-from validation import Validate
-import sqlite3
-
-def request_uploads(playlist_ID = None):
-	data = {}
-	if playlist_ID == None:
-		''' Check the database '''
-		pass
-	else:
-		link = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId={playlist_ID}&key={apikey}".format(playlist_ID = playlist_ID, apikey= apikey)
-		r = requests.get(link)
-		response = r.json()["items"]
-		for i in response:
-			date = i["snippet"]["publishedAt"][0:10]
-			videoID = i["snippet"]["resourceId"]["videoId"]
-			channelTitle = i["snippet"]["channelTitle"]
-			playlistId = i["snippet"]["playlistId"]
-			data.update({date:videoID}) 
-	return data
-
-def download_video(uploads):
-	today = datetime.datetime.now().isoformat()[0:10]
-	ytprefix = "https://www.youtube.com/watch?v="
-	for i in uploads:
-		if i == today:
-			download = ytprefix + uploads[i]
-			print("Downloading " + download)
-			fetch = subprocess.run(["youtube-dl", download], stdout=subprocess.DEVNULL)
-			print("The exit code was: %d" % fetch.returncode)
-
-def new_channel():
-	link = input("Please provide a link: ")
-	playlist_ID = Validate(link)
-	todays_uploads = request_uploads(playlist_ID)
-	download_video(todays_uploads)
-
-	# TODO:
-	# Implement database
-
-
-
-def create_database():
-
-	table_sql = "CREATE TABLE IF NOT EXISTS tweets (youtuber_url TEXT, uploads_url TEXT, youtuber TEXT)"
-
-	conn = sqlite3.connect('yt.db')
-	c = conn.cursor()
-	c.execute(table_sql)
-	c.close()
-	conn.close()
+from validation import *
 
 def main():
 	parser = argparse.ArgumentParser(description="Download Youtube Videos Automatically")
@@ -62,10 +12,38 @@ def main():
 	args = parser.parse_args()
 
 	if args.new:
-		new_channel()
-	else:
-		''' Check creators in database '''
-		pass
+		# Only creates database and inserts new entry.
+		url = User_data()
+		create_db()
+		request_uploads(url)
+	# else:
+	# 	request_uploads()
+	# 	# download_video()
+
+def request_uploads(playlist_id = None):
+	link = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId={playlist_id}&key={apikey}".format(playlist_id = playlist_id, apikey= apikey)
+	r = requests.get(link)
+	response = r.json()["items"]
+	for i in response:
+		channel_title = i["snippet"]["channelTitle"]
+		channel_id = i["snippet"]["channelId"]
+		uploads_id = i["snippet"]["playlistId"]
+		video_id = i["snippet"]["resourceId"]["videoId"]
+		video_date = i["snippet"]["publishedAt"][0:10]
+		insert_data(channel_title, channel_id, uploads_id, video_id, video_date)
+
+def download_video():
+	# Downloads videos based upon video_id
+	uploads = query_db()
+
+	#TO DO: FIX THIS.
+
+	# ytprefix = "https://www.youtube.com/watch?v="
+	# for i in uploads:
+	# 	download = ytprefix + uploads
+	# 	print("Downloading " + download)
+	# 	fetch = subprocess.run(["youtube-dl", download], stdout=subprocess.DEVNULL)
+	# 	print("The exit code was: %d" % fetch.returncode)
 
 if __name__ == "__main__":
 	main()
