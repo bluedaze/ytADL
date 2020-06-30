c338d62aa293e3881166885211081e134a0d64b2
bluedaze <35676690+bluedaze@users.noreply.github.com>
2020-06-29 22:03:11 -0400

    Add files via upload

--- validation.py
+++ validation.py
@@ -4,94 +4,138 @@ import re
 import datetime
 import sqlite3
 
+
 class User_data:
-	''' Checks for valid youtube link '''
-	def __init__(self):
-		self.url = ""
-		self.user_link(self.url)
+    """ Checks for valid youtube link """
 
-	def __str__(self):
-		return "{self.url}".format(self=self)
+    def __init__(self):
+        self.url = ""
+        self.user_link(self.url)
 
-	def user_link(self, url):
-		''' Error message for invalid link. '''
-		message = '''To create an entry we need the channel url.
-		\n\nHere is an example using Google's youtube channel: https://www.youtube.com/channel/UCK8sQmJBp8GCxrOtXWBpyEA''' 
+    def __str__(self):
+        return "{self.url}".format(self=self)
 
-		for c in message:
-			sys.stdout.write(c)
-			sys.stdout.flush()
-			time.sleep(.009)
-		self.url = input("\n\nPlease provide a similar link: ")
-		self.validate_link()
+    def user_link(self, url):
+        """ Error message for invalid link. """
+        message = """To create an entry we need the channel url.
+		\n\nHere is an example using Google's youtube channel: https://www.youtube.com/channel/UCK8sQmJBp8GCxrOtXWBpyEA"""
 
-	def validate_link(self):
-		''' Validate if this is a url by sending a request to the url. '''
-		regex = re.compile(r"(youtube.com/channel/UC)(.{22})")
-		mo = regex.search(self.url)
+        for c in message:
+            sys.stdout.write(c)
+            sys.stdout.flush()
+            time.sleep(0.009)
+        self.url = input("\n\nPlease provide a similar link: ")
+        self.validate_link()
 
-		if mo == None:
-			print("~~~~~~~~~~~~~~~\n\n\nThat is not the link we are looking for.\n")
-			self.user_link(self.url)
-		else:
-			# https://www.youtube.com/feeds/videos.xml?channel_id=UCuXy5tCgEninup9cGplbiFw
-			self.url = mo.group()[20::]
+    def validate_link(self):
+        """ Validate if this is a url by sending a request to the url. """
+        regex = re.compile(r"(youtube.com/channel/UC)(.{22})")
+        mo = regex.search(self.url)
+
+        if mo == None:
+            print("~~~~~~~~~~~~~~~\n\n\nThat is not the link we are looking for.\n")
+            self.user_link(self.url)
+        else:
+            # https://www.youtube.com/feeds/videos.xml?channel_id=UCuXy5tCgEninup9cGplbiFw
+            self.url = mo.group()[20::]
+
 
 def insert_data(*args):
-	# Inserts data from parse_ytrss()
-	insert_Sql = "INSERT INTO ytadl (channel_title, channel_id, uploads_id, video_id, video_date, video_title, description, downloaded) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
-	conn = sqlite3.connect('ytadl.db')
-	c = conn.cursor()
-	try:
-		c.execute(insert_Sql, args)
-	except sqlite3.IntegrityError:
-		pass
-	conn.commit()
-	c.close()
-	conn.close()
+    # Inserts data from parse_ytrss()
+    insert_Sql = "INSERT INTO ytadl (channel_title, channel_id, uploads_id, video_id, video_date, video_title, description, downloaded) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+    conn = sqlite3.connect("ytadl.db")
+    c = conn.cursor()
+    try:
+        c.execute(insert_Sql, args)
+    except sqlite3.IntegrityError:
+        pass
+    conn.commit()
+    c.close()
+    conn.close()
+
 
 def create_db():
-	# Creates a database if it does not exist.
-	table_sql = "CREATE TABLE IF NOT EXISTS ytadl (channel_title TEXT, channel_id TEXT, uploads_id TEXT, \
-	video_id TEXT NOT NULL UNIQUE, video_date TEXT, video_title TEXT, description TEXT, downloaded TEXT)"
+    # Creates a database if it does not exist.
 
-	conn = sqlite3.connect('ytadl.db')
-	c = conn.cursor()
-	c.execute(table_sql)
-	c.close()
-	conn.close()
+    conn = sqlite3.connect("ytadl.db")
+    c = conn.cursor()
+    c.execute(
+        "CREATE TABLE IF NOT EXISTS ytadl (channel_title TEXT, channel_id TEXT, uploads_id TEXT, video_id TEXT NOT NULL UNIQUE, video_date TEXT, video_title TEXT, description TEXT, downloaded TEXT)"
+    )
+    c.close()
+    conn.close()
+
 
 def query_db():
-	# Returns values from database.
-	conn = sqlite3.connect("ytadl.db")
-	conn.row_factory = sqlite3.Row
-	c = conn.cursor()
-	c.execute("SELECT channel_title, channel_id, uploads_id, video_id, video_date, video_title, downloaded FROM ytadl where downloaded != '1';")
-	selected = [tuple(row) for row in c.fetchall()]
-	c.close()
-	conn.close()
-	return selected
+    # Returns values from database.
+    conn = sqlite3.connect("ytadl.db")
+    conn.row_factory = sqlite3.Row
+    c = conn.cursor()
+    c.execute(
+        "SELECT channel_title, channel_id, uploads_id, video_id, video_date, video_title, downloaded FROM ytadl where downloaded != '1';"
+    )
+    selected = [tuple(row) for row in c.fetchall()]
+    c.close()
+    conn.close()
+    return selected
+
 
 def query_creators():
-	# Returns values from database.
-	conn = sqlite3.connect("ytadl.db")
-	conn.row_factory = sqlite3.Row
-	c = conn.cursor()
-	c.execute("SELECT channel_title, channel_id, uploads_id, video_id, video_date, video_title, downloaded FROM ytadl")
-	selected = [tuple(row) for row in c.fetchall()]
-	c.close()
-	conn.close()
-	return selected
+    # Returns values from database.
+    conn = sqlite3.connect("ytadl.db")
+    conn.row_factory = sqlite3.Row
+    c = conn.cursor()
+    c.execute(
+        "SELECT channel_title, channel_id, uploads_id, video_id, video_date, video_title, downloaded FROM ytadl"
+    )
+    selected = [tuple(row) for row in c.fetchall()]
+    c.close()
+    conn.close()
+    return selected
+
+
+def query_videos():
+    # Returns values from database.
+    conn = sqlite3.connect("ytadl.db")
+    conn.row_factory = sqlite3.Row
+    c = conn.cursor()
+    c.execute("SELECT channel_title, video_date, video_title FROM ytadl")
+    selected = [tuple(row) for row in c.fetchall()]
+    c.close()
+    conn.close()
+    return selected
+
 
 def mark_downloaded(video_id):
-	# marks videos as downloaded.
-	conn = sqlite3.connect("ytadl.db")
-	c = conn.cursor()
-	c.execute("UPDATE ytadl SET downloaded = 1 WHERE video_id = ?", (video_id,))
-	conn.commit()
-	c.close()
-	conn.close()
+    # marks videos as downloaded.
+    conn = sqlite3.connect("ytadl.db")
+    c = conn.cursor()
+    c.execute("UPDATE ytadl SET downloaded = 1 WHERE video_id = ?", (video_id,))
+    conn.commit()
+    c.close()
+    conn.close()
+
+def backup_query():
+    # Returns values from database.
+    conn = sqlite3.connect("ytadl.db")
+    conn.row_factory = sqlite3.Row
+    c = conn.cursor()
+    c.execute("SELECT * FROM ytadl")
+    selected = [tuple(row) for row in c.fetchall()]
+    c.close()
+    conn.close()
+    with open('ytadl_backup.txt', 'w') as f:
+        for i in selected:
+            channel_title = i[0]
+            channel_id = i[1]
+            uploads_id = i[2]
+            video_id = i[3]
+            video_date = i[4]
+            video_title = i[5]
+            description = i[6]
+            downloaded = i[7]
+            print(f"{channel_title},{channel_id},{uploads_id},{video_id},{video_date},{video_title},{downloaded}", file=f)
 
 
 if __name__ == "__main__":
-	User_data()
+    User_data()
--- ytautodl.py
+++ ytautodl.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 # Guide for getting details from youtube videos for future reference.
 # https://developers.google.com/youtube/v3/guides/implementation/videos
-# TODO: Allow users to see which videos have been downloaded.
-# TODO: Allow users to play video from terminal. Possibly use mplayer?
-# TODO: Allow users to set what time task is scheduled. Unsure how to implement this. Would prefer crossplatform, so crontab may be out of the picture.
+# TODO: Allow users to play video from terminal. Possibly use mpv?
 # TODO: Allow users to download videos from search.
 # TODO: Show thumbnails in search.
-# TODO: Export data
-# TODO: Allow user to specify arguments for youtubel-dl, for example whether or not to use aria2.
+# TODO: Allow users to add channel by name.
+# TODO: Create argument that will allow you to add from CSV.
+# TODO: Show related after videos are done downloading.
+# TODO: Allow users to receive email notifcations.
 
 # CHANGELOG: Sorta? idk? This is is what I am going to keep track of what has been changed.
-# 1.) Created new function to parse channel rss feeds instead of calling the youtube api.
-# 2.) It should be illegal to use this many ansi escape codes. I'll have to clean this up later.
-# 3.) Decreased visible clutter while running the program.
-# 4.) Started using aria2 to increase speeds of downloads.
-# 5.) Updated database with description column, and downloaded column. Queries marked as downloaded will not be returned from query_db()
-# 6.) Added mark_downloaded query. This will reduce queried results returned, and number of requests to youtube.
-# 7.) Added kickass ascii art.
-
+# 1.) Removed old function request_uploads()
+# 2.) Built framework for additional arguments, though some currently are not implemented entirely.
+# 3.) Implemented function which shows you all videos that have been downloaded.
+# 4.) Users may now specify their own youtube-dl functions.
+# 5.) Adding a new youtuber now shows confirmation, and the name of the youtuber that was added.
+# 6.) All flags are now moved to their own functions.
+# 7.) Major reformat of code, now implemented Python black to reformat code.
+# 8.) All important data can be backed up in a text file. Though I have not implemented a way to add data back to a database, yet. Ha!
 
 import requests
 import subprocess
@@ -26,198 +26,20 @@ from apikey import apikey
 from validation import *
 import os
 import feedparser
+import time
 
 color_green = "\u001b[38;5;46m"
-removecolor = "\u001b[0m"
+stripformatting = "\u001b[0m"
 underline = "\u001b[4m"
+clearline = "\033[K"
+moveup = "\033[1A"
+movedown = "\033[1B"
+returnhome = "\u001b[1000D"
+bold = "\u001b[1m"
+save = "\u001b[1s"
+goback = "\u001b[1u"
 
-
-def main():
-	parser = argparse.ArgumentParser(description="Download Youtube Videos Automatically")
-	parser.add_argument("-new", action='store_true', help="Add a youtube creator to the database.")
-	parser.add_argument("-d", action='store_true', help="Checks database and downloads videos automatically")
-	parser.add_argument("-channels", action='store_true', help="Shows you all channels that have been added to the database.")
-	parser.add_argument("-search", action='store_true', help="Search youtube")
-	args = parser.parse_args()
-
-	if args.new:
-		# Only creates database and inserts new entry.
-		#TODO: Show confirmation messages which displays which channel was added to database.
-		#TODO: Allow users to add channel by name.
-		
-		url = User_data()
-		create_db()
-		parse_ytrss(url)
-	
-	elif args.d:
-		# Query the database, create a set of unique ids, send a request for each unique id.
-		
-		uploads = query_creators()
-		uid_set = set()
-
-		for i in uploads:
-			uid_set.add(i[1])
-		for i in uid_set:
-			parse_ytrss(i)
-
-		download_video()
-
-	elif args.search:
-		search_results()
-
-	elif args.channels:
-		print(f"{underline} {color_green} Your Channels: {removecolor}")
-		channel_set = set()
-		data = query_db()
-
-		for i in data:
-			channel_set.add(i[0])
-		for i in channel_set:
-			print(f"{color_green} {i} {removecolor}\n\n")
-	else:
-		print(f"{color_green} You did not specify arguments. Type ytautodl.py -h for more information on how to run this program\n{removecolor}")
-
-	#TODO: Create argument that will allow you to add from CSV.
-	#TODO: Show related after videos are done downloading.
-
-def request_uploads(playlist_id):
-	# Deprecated. Reserving this method just in case youtube gets rid of their rss feed.
-	params = {
-		"part": "snippet",
-		"playlistId": playlist_id,
-		"key": api_key
-		}
-
-
-	link = "https://www.googleapis.com/youtube/v3/playlistItems"
-	r = requests.get(link, params=params)
-	response = r.json()["items"]
-	for i in response:
-		channel_title = i["snippet"]["channelTitle"]
-		channel_id = i["snippet"]["channelId"]
-		uploads_id = i["snippet"]["playlistId"]
-		video_id = i["snippet"]["resourceId"]["videoId"]
-		video_date = i["snippet"]["publishedAt"][0:10]
-		video_title = i["snippet"]["title"]
-		insert_data(channel_title, channel_id, uploads_id, video_id, video_date, video_title)
-
-def parse_ytrss(rss_link):
-	# New function will parse youtube rss feeds to reduce api requests.
-	#TODO: Skip videos which have been marked as downloaded.
-	rss_link = f"https://www.youtube.com/feeds/videos.xml?channel_id={rss_link}"
-
-
-	r = requests.get(rss_link)
-	page = r.text
-
-	d = feedparser.parse(page)
-	entries = d.entries
-
-	for i in entries:
-		channel_title = i['authors'][0]['name'] 
-		channel_id = i['yt_channelid'] 
-		uploads_id = "UU" + channel_id[2::]
-		video_id = i['yt_videoid'] 
-		video_date = i['published'][0:10]
-		video_title = i['title'] 
-		description = i['summary']
-		downloaded = '0'
-		insert_data(channel_title, channel_id, uploads_id, video_id, video_date, video_title, description, downloaded)
-
-
-def search_results():
-	videos = []
-	search_query = input("Search: ")
-	search_query  = search_query.replace(' ', '%20')
-	linesep = f"*{'-'*90}"
-	params = {
-		"part": "snippet",
-		"maxResults": 5,
-		"q": search_query,
-		"key": api_key
-	}
-	link = "https://www.googleapis.com/youtube/v3/search"
-	r = requests.get(link, params=params)
-	response = r.json()
-	count = 0
-	for i in response["items"]:
-		count = count + 1
-		channelTitle = i["snippet"]["channelTitle"]
-		videoTitle = i["snippet"]["title"]
-		description = i["snippet"]["description"]
-		publishTime = i["snippet"]["publishTime"]
-		try:
-			video_id = i["id"]["videoId"]
-			videos.append(video_id)
-			print('\n\n')
-			print(linesep.replace("*", str(count)))
-			print("| Channel:", channelTitle)
-			print("| Video:", videoTitle)
-			if description == "":
-				print("| Description: None")
-			else:
-				print("| Description:", description)
-			print("| Uploaded:", publishTime)
-			print(linesep)
-		except KeyError:
-			count -= 1
-			pass
-
-	print("totalResults:", response["pageInfo"]["totalResults"])
-	print("resultsPerPage:", response["pageInfo"]["resultsPerPage"])
-	choose_video = int(input("Enter a number to select which video you would like to see: "))
-	choose_video -= 1
-	print(choose_video)
-	try:
-		print(f"https://www.youtube.com/watch?v={videos[choose_video]}")
-	except IndexError:
-		print("Not a valid number")
-	print(videos)
-
-def download_video():
-	channelName = ""
-	print("Searching for content...")
-	uploads = query_creators()
-	ytprefix = "https://www.youtube.com/watch?v="
-	for i in uploads:
-		channel_title = i[0]
-		video_id = i[3]
-		video_date = i[4]
-		video_title = i[5]
-		path = os.getcwd() + "/" + channel_title
-		download = ytprefix + video_id
-		if os.path.isdir(path) == False:
-				os.mkdir(path)
-				print ("Directory %s created" % path)
-		clearline = "\033[K"
-		moveup = "\033[1A"
-		movedown = "\033[1B"
-		returnhome = "\u001b[1000D"
-		bold = "\u001b[1m"
-
-		os.chdir(path)
-		if channelName != channel_title:
-		#TODO: Only print name once, until new youtube is added.
-			channelName = channel_title
-			print(f"{moveup}{returnhome}{clearline}{bold}{color_green}Channel: {channel_title}{removecolor}")
-		# sys.stdout.write(clearline)
-		print(f"{returnhome}{clearline}{color_green}Video: {video_title}{removecolor}", end="")
-		# print(f"{returnhome}", end ="")
-		sys.stdout.flush()
-		# sys.stdout.flush()
-		# sys.stdout.write(returnhome)
-		# sys.stdout.write(clearline)
-		# sys.stdout.write(moveup)
-		# sys.stdout.write(clearline)
-		fetch = subprocess.run(["youtube-dl", "--no-warnings", "-o", "%(uploader)s - %(title)s.%(ext)s", "--external-downloader", "aria2c", "--external-downloader-args", "'-x 8 -s 8 -k 1m'", download], stdout=subprocess.DEVNULL)
-		os.chdir("..")
-		if fetch.returncode == 0:
-			mark_downloaded(video_id)
-		else:
-			continue
-
-if __name__ == "__main__":
-	ytadl = '''                                                                              
+ytadl = '''                                                                              
                                                                               
                      mm         db      `7MM"""Yb. `7MMF'                     
                      MM        ;MM:       MM    `Yb. MM                       
@@ -230,6 +52,253 @@ if __name__ == "__main__":
          OOb                                                                   
                                                                               
 '''
-	sys.stdout.write(f"{color_green} {ytadl} {removecolor}")
-	main()
-	print(f"\n\n{color_green}Thank you for using ytautodl...{removecolor}\n\n")
+
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        description="A wrapper for youtube-dl that adds additional features."
+    )
+
+    parser.add_argument(
+        "-dp",
+        action="store_true",
+        help="Set default download path, else downloads in current location",
+    )
+    parser.add_argument(
+        "-f", action="store", nargs="+", help="Set custome flags for youtube-dl",
+    )
+    parser.add_argument("-a", action="store_true", help="Add youtube api key")
+    parser.add_argument("-new", action="store_true", help="Add a new youtuber.")
+    parser.add_argument(
+        "-d", action="store_true", help="Download new videos.",
+    )
+    parser.add_argument(
+        "-c", action="store_true", help="Display channels stored in database.",
+    )
+    parser.add_argument("-p", action="store_true", help="List your preferences")
+    parser.add_argument(
+        "-v", action="store_true", help="Display downloaded videos",
+    )
+    parser.add_argument("-search", action="store_true", help="Search youtube")
+    parser.add_argument("-b", action="store_true", help="Send db info to txt file.")
+    parser.add_argument(
+        "-silent", action="store_true", help="Run without print to the cli."
+    )
+    parser.add_argument(
+        "-ep", action="store_true", help="enable plex, and set plex options"
+    )
+    args = parser.parse_args()
+
+    if args.new:
+        new()
+
+    elif args.d:
+        download_video()
+
+    elif args.search:
+        search_results()
+
+    elif args.c:
+        channels()
+
+    elif args.p:
+        pass
+
+    elif args.v:
+        videos()
+
+    elif args.f:
+        download_video(args.f)
+    elif args.b:
+        backup_query()
+
+    else:
+        print(
+            f"{color_green} You did not specify arguments. Type ytautodl.py -h for more information on how to run this program\n{stripformatting}"
+        )
+
+
+def new():
+    # Only creates database and inserts new entry.
+
+    url = User_data()
+    create_db()
+    # Returns details from the feed. Currently only returns youtube name.
+    details = parse_ytrss(url)
+
+    print(f"\n\n\n\t{details} added to database")
+
+
+def channels():
+    print(f"{underline} {color_green} Your Channels: {stripformatting}")
+    channel_set = set()
+    data = query_db()
+
+    for i in data:
+        channel_set.add(i[0])
+    for i in channel_set:
+        print(f"{color_green} {i} {stripformatting}\n\n")
+
+
+def videos():
+    videos = []
+    data = query_videos()
+    for i in data:
+        channel = i[0].ljust(40)
+        video = i[2].ljust(40)
+        entry = channel + video
+        videos.append(entry)
+    mylist = videos.sort()
+    for i in videos:
+        print(i)
+
+
+def parse_ytrss(rss_link):
+    """ Fetch rss feed from youtube, and parse feed to look for new videos """
+    rss_link = f"https://www.youtube.com/feeds/videos.xml?channel_id={rss_link}"
+
+    r = requests.get(rss_link)
+    page = r.text
+
+    d = feedparser.parse(page)
+    entries = d.entries
+
+    for i in entries:
+        channel_title = i["authors"][0]["name"]
+        channel_id = i["yt_channelid"]
+        uploads_id = f"{'UU'}{channel_id[2::]}"
+        video_id = i["yt_videoid"]
+        video_date = i["published"][0:10]
+        video_title = i["title"]
+        description = i["summary"]
+        downloaded = "0"
+        insert_data(
+            channel_title,
+            channel_id,
+            uploads_id,
+            video_id,
+            video_date,
+            video_title,
+            description,
+            downloaded,
+        )
+
+    return channel_title
+
+
+def search_results():
+    """ Uses Google api to search, must supply your own api key """
+    videos = []
+    search_query = input("Search: ")
+    search_query = search_query.replace(" ", "%20")
+    linesep = f"*{'-'*90}"
+    params = {"part": "snippet", "maxResults": 5, "q": search_query, "key": api_key}
+    link = "https://www.googleapis.com/youtube/v3/search"
+    r = requests.get(link, params=params)
+    response = r.json()
+    count = 0
+    for i in response["items"]:
+        count = count + 1
+        channelTitle = i["snippet"]["channelTitle"]
+        videoTitle = i["snippet"]["title"]
+        description = i["snippet"]["description"]
+        publishTime = i["snippet"]["publishTime"]
+        try:
+            video_id = i["id"]["videoId"]
+            videos.append(video_id)
+            print("\n\n")
+            print(linesep.replace("*", str(count)))
+            print("| Channel:", channelTitle)
+            print("| Video:", videoTitle)
+            if description == "":
+                print("| Description: None")
+            else:
+                print("| Description:", description)
+            print("| Uploaded:", publishTime)
+            print(linesep)
+        except KeyError:
+            count -= 1
+            pass
+
+    print("totalResults:", response["pageInfo"]["totalResults"])
+    print("resultsPerPage:", response["pageInfo"]["resultsPerPage"])
+    choose_video = int(
+        input("Enter a number to select which video you would like to see: ")
+    )
+    choose_video -= 1
+    print(choose_video)
+    try:
+        print(f"https://www.youtube.com/watch?v={videos[choose_video]}")
+    except IndexError:
+        print("Not a valid number")
+    print(videos)
+
+
+def download_video(ytargs=None):
+    """ Fetches videos marked as not downloaded, and downloads them """
+
+    print(f"{color_green}Searching for content...{stripformatting}")
+
+    youtubers = query_creators()
+    uid_set = set()
+
+    for i in youtubers:
+        uid_set.add(i[1])
+    for i in uid_set:
+        parse_ytrss(i)
+    # Initialize variable channel. Will be used to prevent reprints of name of channel to the console
+    channelName = ""
+    uploads = query_db()
+    for i in uploads:
+        channel_title = i[0]
+        video_id = i[3]
+        video_date = i[4]
+        video_title = i[5]
+        path = f"{os.getcwd()}{'/'}{channel_title}"
+        download = f"https://www.youtube.com/watch?v={video_id}"
+        if os.path.isdir(path) == False:
+            os.mkdir(path)
+            print(f"Directory {path} created")
+
+        os.chdir(path)
+        if channelName != channel_title:
+            channelName = channel_title
+            print(
+                f"{moveup}{returnhome}{clearline}{color_green}Channel: {bold}{channel_title}{stripformatting}"
+            )
+        print(
+            f"{returnhome}{clearline}{color_green}Video: {video_title}{stripformatting}",
+            end="",
+        )
+        sys.stdout.flush()
+
+        flags = [
+            "youtube-dl",
+            "--no-warnings",
+            "-i",
+            "-o",
+            "%(uploader)s - %(title)s.%(ext)s",
+            "--external-downloader",
+            "aria2c",
+            "--external-downloader-args",
+            "'-x 8 -s 8 -k 1m'",
+            download,
+        ]
+
+        if ytargs is None:
+            pass
+        else:
+            flags = ytargs
+
+        fetch = subprocess.run(flags, stdout=subprocess.DEVNULL,)
+        os.chdir("..")
+
+        if fetch.returncode == 0:
+            mark_downloaded(video_id)
+
+
+if __name__ == "__main__":
+    print(f"{color_green} {ytadl} {stripformatting}")
+    main()
+    print(f"{color_green}\n\nThank you for using ytautodl...{stripformatting}\n\n")
